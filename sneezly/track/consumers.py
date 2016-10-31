06cdc88654379b7be0791c5b0d0037fdeb5785cc
Elliot Marsden <elliot.marsden@gmail.com>
2016-11-01 00:07:30 +0100

    Allow setting attributes, and time in chat

--- sneezly/track/consumers.py
+++ sneezly/track/consumers.py
@@ -1,8 +1,13 @@
 import json
 
 from channels import Channel, Group
+import parsedatetime
 
 from . import models
+from . import forms
+
+
+calendar = parsedatetime.Calendar()
 
 
 def slack_connect(message):
@@ -13,18 +18,49 @@ def slack_connect(message):
     })
 
 
+def chunks(lst, chunk_size=2):
+    return (lst[i:i + n] for i in range(0, len(lst), n))
+
+
 def slack_message(message):
     target = Channel('slack.send')
     data = json.loads(message.content['text'].decode('utf-8'))
     slack_channel = data['channel']
+
+    def send(s):
+        target.send({
+            'text': s,
+            'channel': slack_channel,
+        })
+
+    form_data = {}
+
     text = data['text']
-    if any(s in text for s in ['sneeze', 'achoo']):
-        SneezeEvent = models.EventType.objects.get(name__contains='neeze')
-        ev = models.Event.objects.create(type=SneezeEvent)
-        reply_text = 'Bless you! I heard you sneeze at {}'.format(ev.time)
+    words = text.split()
+    event_name = words[0]
+    pairs = [s.split('=') for s in words[1:]]
+
+    try:
+        event_type = models.EventType.objects.get(name__iexact=event_name)
+    except models.EventType.DoesNotExist:
+        send("Cannot find event type '{}'".format(event_name))
+        return
     else:
-        reply_text = 'huh?'
-    target.send({
-        'text': reply_text,
-        'channel': slack_channel,
-    })
+        form_data['type'] = event_type.pk
+
+    if pairs:
+        attrs = {}
+        for key, value in pairs:
+            if key == '@':
+                form_data['time'] = calendar.parseDT(value)[0]
+            else:
+                attrs[key] = value
+        form_data['attrs'] = json.dumps(attrs)
+
+    form = forms.EventForm(data=form_data)
+    if form.is_valid():
+        ev = form.save()
+        send('I heard a {} at {}'.format(ev.type.name, ev.time))
+    else:
+        send('Hmm, I do not understand:')
+        send(json.dumps(form.errors))
---
+++ sneezly/track/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from . import models
+
+
+class EventForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Event
+        fields = ['type', 'attrs', 'notes', 'time']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['time'].required = False
