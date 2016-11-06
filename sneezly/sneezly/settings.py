fc9f06147a8a8f46f5a8e8a3b06d4218f42e9047
Elliot Marsden <elliot.marsden@gmail.com>
2016-11-06 16:11:11 +0100

    Add options to deploy to AWS

--- sneezly/sneezly/settings.py
+++ sneezly/sneezly/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config.get('secrets', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.getboolean('debug', 'DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
