a1b64fbc1d5d26b5343bbde8f7ab01f12b7c55d0
Elliot Marsden <elliot.marsden@gmail.com>
2016-11-06 16:19:44 +0100

    set STATIC_ROOT setting

--- sneezly/sneezly/settings.py
+++ sneezly/sneezly/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
