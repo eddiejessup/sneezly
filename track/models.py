61d669dbd22cf81b55e02e6433da5d0f4b4b1550
Elliot Marsden <elliot.marsden@gmail.com>
2016-11-07 22:24:42 +0100

    Add modified and created times to events

--- track/models.py
+++ track/models.py
@@ -49,6 +49,8 @@ class Event(models.Model):
     time = models.DateTimeField(default=timezone.now)
     attrs = JSONField(default=dict, blank=True)
     notes = models.TextField(blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '<{} type={}, time={}>'.format(self.__class__.__name__,
